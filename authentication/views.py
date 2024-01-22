182d38bbeca1faaeed5739c85b48408c3ac493a5
Cero72 <110662485+Cero72@users.noreply.github.com>
2024-01-22 12:29:58 +0530

    have set up the email verification, works as indented

--- authentication/views.py
+++ authentication/views.py
@@ -5,7 +5,14 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from devz import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from .tokens import generate_token
+
+
 
 
 def home(request):
@@ -42,16 +49,38 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        
+        myuser.is_active = False
         myuser.save()
         messages.success(request, "Your account has been successfully created. We have sent you a confirmation E-mail, please confirm your email to activate your email." )
 
         #Welcome Email
         subject = "Welcome to Devzery"
-        message = "Hello" + myuser.first_name + "! \n" + "Welcome to Devzery \n Thank you for visiting our website \n We have also sent you a confirmation email, please confirm yout email address in order to activate your activate your account. \n Thank You" 
+        message = "Hello" + myuser.first_name + "! \n" + "Welcome to Devzery \n Thank you for visiting our website \n We have also sent you a confirmation email, please confirm your email address in order activate the account. \n Thank You" 
         from_email = settings.EMAIL_HOST_USER
         to_list = [myuser.email]
         send_mail(subject, message, from_email, to_list, fail_silently = True)
+
+
+        # Email address confirmation Email
+
+        current_site = get_current_site(request)
+        email_subject = "Confirm your email @ Devzery Login! "
+        message2 = render_to_string('email_confirmation.html', {
+            'name' : myuser.first_name,
+            'domain': current_site.domain,
+            'uid' : urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token' : generate_token.make_token(myuser)
+
+        })
+        email = EmailMessage(
+            email_subject, 
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+
 
         return redirect('signin')
 
@@ -80,3 +109,17 @@ def signout(request):
     messages.success(request, "Logged Out successfully")
     return redirect('home')
 
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk = uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser, token):
+        myuser.is_active = True
+        myuser.save()
+        login(request, myuser)
+        return redirect('home')
+    else:
+        return render(request, 'activation_failed.html')
